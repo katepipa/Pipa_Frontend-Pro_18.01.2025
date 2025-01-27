caa04c50098e950958d719ebe6f8092fde8ac075
Kateryna Pipa <katepipa12@gmail.com>
2025-01-28 00:20:35 +0100

    Code was finished for homework- 2

--- converter.js
+++ converter.js
@@ -1,5 +1,5 @@
 const SECONDS_PER_HOUR = 3600;
-let numberOfHours = prompt('Hi! Enter the number of hours');
-console.log(numberOfHours);
-let amountOfSeconds = SECONDS_PER_HOUR * numberOfHours;
-console.log(alert('The number of seconds in ', numberOfHours, 'hours is ', amountOfSeconds)); 
+let userInput = prompt('Hi! Enter the number of hours');
+let numberOfHours = Number(userInput);
+let numberOfSeconds = SECONDS_PER_HOUR * numberOfHours;
+alert('The number of seconds in ' + numberOfHours + ' hours is ' + numberOfSeconds); 
