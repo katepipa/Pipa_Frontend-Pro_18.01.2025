6af90522710ce95482d9ef0b4804c6de257ba2be
Kateryna Pipa <katepipa12@gmail.com>
2025-01-27 22:42:37 +0100

    File was removed to root

---
+++ converter.js
@@ -0,0 +1,5 @@
+const SECONDS_PER_HOUR = 3600;
+let numberOfHours = prompt('Hi! Enter the number of hours');
+console.log(numberOfHours);
+let amountOfSeconds = SECONDS_PER_HOUR * numberOfHours;
+console.log(alert('The number of seconds in ', numberOfHours, 'hours is ', amountOfSeconds)); 
