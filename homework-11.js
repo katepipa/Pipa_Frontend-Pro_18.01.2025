2907ca9b4ad40e229978de2b6d2c9a100916315c
Kateryna Pipa <katepipa12@gmail.com>
2025-02-24 18:26:15 +0100

    Practice obj

---
+++ homework-11.js
@@ -0,0 +1,18 @@
+let obj = {
+  id: 4,
+  name: 'Kate',
+};
+
+obj.name
+obj.name = 'Svitlana';
+
+const arrayOfUser = [{
+  id: 1,
+  name: 'Kate',
+}, {
+  id: 2,
+  name: 'Luka',
+}, {
+  id: 3,
+  name: 'Lisa',
+}];
