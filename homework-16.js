ba472ef36333ebea4dbe42bb9efd817db17b3403
Kateryna Pipa <katepipa12@gmail.com>
2025-03-13 15:52:19 +0100

    Code for HW16 was finished

--- homework-16.js
+++ homework-16.js
@@ -1,12 +1,37 @@
-function fib(n) {
-  if (!fib.cache) {
-    fib.cache = {};
-  }
+function saveCalls (originalFunction) {
+  const cache = {};
+  const lastTenCallsArray = [];
 
-  if (fib.cache[n] === undefined) {
-    fib.cache[n] = ;
-  }
+  return function (phoneNumber) {
+    if (cache[phoneNumber] !== undefined) {
+      console.log(`This phone number was retrieved from cache: ${phoneNumber}`);
 
-  return fib.cache[n];
+      return cache[phoneNumber];
+    }
+
+    const result = originalFunction(phoneNumber);
+    cache[phoneNumber] = result;
+    lastTenCallsArray.push(phoneNumber);
+
+    if (lastTenCallsArray.length > 10) {
+      const oldestPhoneNumber = lastTenCallsArray.shift();
+      delete cache[oldestPhoneNumber];
+    }
+
+    console.log(`New call was completed: ${phoneNumber}`);
+
+    return result;
+  }
 }
 
+function makeCall (phoneNumber) {
+  return `A call was made to the number: ${phoneNumber}`;
+}
+
+const cachedCall = saveCalls(makeCall);
+
+console.log(cachedCall('056-123-19-07')); 
+console.log(cachedCall('056-123-19-07')); 
+console.log(cachedCall('125-923-23-81')); 
+console.log(cachedCall('125-923-23-81')); 
+console.log(cachedCall('123-642-12-56'));
